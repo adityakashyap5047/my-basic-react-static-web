be4194a5fad5a82888121628cd75a92229852433
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-06-18 06:19:53 +0530

    basic introduction about me

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+    <>
+      <h1>Static React web app</h1>
+      <ul>
+        <li><h3>Hello everyone, Myself Aditya kumar. I am currently pursuing B.Tech in CSE(AI).</h3></li>
+          <ul>
+            <li><p>I have glad to inform you that I have a good knowledge of Full Stack development in Mern.</p></li>
+            <li><p>I am also completed many project in this technology.</p></li>
+          </ul>
+      </ul>
+      <p>I maked the simple static-react-web-app and deploy it with the help of microsoft azure.</p>
+    </>
+  );
 }
 
 export default App;
